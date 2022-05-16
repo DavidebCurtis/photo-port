e73a12d6546d3e1180ebb949b459f4b00877bf3d
david curtis <heythereimdavidcurtis@gmail.com>
2022-05-16 15:52:03 -0400

    modal tests

---
+++ src/components/Modal/__tests__/index.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Modal from '../';
+
+const currentPhoto = {
+  name: 'Park bench',
+  category: 'landscape',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+  index: 1,
+};
+
+describe('Modal component', () => {
+  it('renders', () => {
+    render(<Modal />);
+  });
+  it('renders', () => {
+    const { asFragment } = render(<Modal />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
